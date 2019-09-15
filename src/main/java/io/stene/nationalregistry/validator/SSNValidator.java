7722ffe28f4cbf2ecd01d9776233a0192b111be1
Henrik Stene <hstene@me.com>
2019-09-15 15:55:25 +0200

    Added validation of day of month

--- src/main/java/io/stene/nationalregistry/validator/SSNValidator.java
+++ src/main/java/io/stene/nationalregistry/validator/SSNValidator.java
@@ -6,11 +6,20 @@ public class SSNValidator {
 
     public static void validate(String ssn) {
         validateSsnLength(ssn);
+        validateDayOfMonth(ssn);
     }
 
     private static void validateSsnLength(String ssn) {
         if (ssn.length() != 11) {
             throw new ValidationException("Invalid SSN");
+        }
+    }
+
+    private static void validateDayOfMonth(String ssn) {
+        int day = Integer.parseInt(ssn.substring(0,2));
+
+        if (day < 0 || day > 31) {
+            throw new ValidationException("Day of month is out of range");
         }
     }
 }
