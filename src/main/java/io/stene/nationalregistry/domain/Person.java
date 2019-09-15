3bbb03ac9798c8ff146dc64634acb276355a40d1
Henrik Stene <hstene@me.com>
2019-09-15 14:29:26 +0200

    Added annotations to person

--- src/main/java/io/stene/nationalregistry/domain/Person.java
+++ src/main/java/io/stene/nationalregistry/domain/Person.java
@@ -1,8 +1,14 @@
 package io.stene.nationalregistry.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
     Integer id;
     String ssn;
