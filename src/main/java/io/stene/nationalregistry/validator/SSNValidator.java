4c38b54bb1ad399cb47382d7c965bab3c33464a0
Henrik Stene <hstene@me.com>
2019-09-16 19:46:36 +0200

    Add validation skeleton

--- src/main/java/io/stene/nationalregistry/validator/SSNValidator.java
+++ src/main/java/io/stene/nationalregistry/validator/SSNValidator.java
@@ -6,7 +6,7 @@ public class SSNValidator {
 
     public static void validate(String ssn) {
         validateSsnLength(ssn);
-        validateDayOfMonth(ssn);
+        // Add validation of day of month here
     }
 
     private static void validateSsnLength(String ssn) {
@@ -18,7 +18,7 @@ public class SSNValidator {
     private static void validateDayOfMonth(String ssn) {
         int day = Integer.parseInt(ssn.substring(0,2));
 
-        if (day < 1 || day > 71) {
+        if (day < 1 || day > 31) {
             throw new ValidationException("Day of month is out of range");
         }
     }
